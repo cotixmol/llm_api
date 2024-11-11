a1923a11b361a9afde5fa5850f645a7b64bc5926
MartinBarran <marrandeguy@gmail.com>
2024-11-11 14:53:50 -0300

    [DAT-135]FIX: fixed create_topic_and_summary parameter

--- core/repositories/bertopic_repository.py
+++ core/repositories/bertopic_repository.py
@@ -117,7 +117,7 @@ class BertopicRepository:
             topic_docs = doc_info[doc_info['Topic'] == topic].sort_values('Probability', ascending=False)
 
             # try:
-            name, description = self.llm_repository.create_topic_name_and_summary(num_topics= 8, 
+            name, description = self.llm_repository.create_topic_name_and_summary(num_keywords= 8, 
                                                                             num_docs= 8, 
                                                                             keywords = valid_words, 
                                                                             docs_list= topic_docs["Document"].tolist())
