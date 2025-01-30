3b1d9cbd8415381bd3f08a839bcb5e7302537d26
MartinBarran <marrandeguy@gmail.com>
2025-01-30 11:11:20 -0300

    [DAT-175]Debugging

--- core/cases/get_summary_response.py
+++ core/cases/get_summary_response.py
@@ -57,7 +57,8 @@ class GetSummaryResponseCase:
         )
         self.query_repository.set_match_by_field(field="content")
         self.query_repository.set_match_by_field(field=self.summary_field)
-        if self.query and self.query.strip():  # Asegura que no sea None ni un string vacío
+        print(f"DEBUG: self.query = {repr(self.query)}, type = {type(self.query)}")
+        if self.query is not None:  
             self.query_repository.set_query_string(query_string=f'(NOT category.keyword: "Streaming") AND (content_type.keyword: ("Post" OR "tweet" OR "New" OR "videos" OR "shorts")) AND ({self.query})')
         else:
             self.query_repository.set_query_string(query_string='(NOT category.keyword: "Streaming") AND (content_type.keyword: ("Post" OR "tweet" OR "New" OR "videos" OR "shorts"))')
