99b58f1eb2e4844f29a61efbbfdc3cfe34dc6daf
lucas.rohatsch <lucas.rohatsch@reputaciondigital.info>
2025-02-07 08:56:36 -0300

    Implements pydantic-settings

--- api/config/secrets.py
+++ api/config/secrets.py
@@ -1,19 +1,27 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
-load_dotenv('.env')
 
-LOG_LEVEL = os.environ.get("LOG_LEVEL")
+class Settings(BaseSettings):
 
-ELASTIC_PRT = os.environ.get("ELASTIC_PRT")
-ELASTIC_USR = os.environ.get("ELASTIC_USR")
-ELASTIC_PSW = os.environ.get("ELASTIC_PSW")
-ELASTIC_CLUSTER = os.environ.get("ELASTIC_CLUSTER")
-ELASTIC_PAGE_SIZE = os.environ.get("ELASTIC_PAGE_SIZE")
-ELASTIC_TIMEOUT = os.environ.get("ELASTIC_TIMEOUT")
+    # Worker configuration
+    LOG_LEVEL: str
+    MODEL_NAME: str
 
-MINIO_URL=os.environ.get("MINIO_URL")
-MINIO_ACCESS_KEY=os.environ.get("MINIO_ACCESS_KEY")
-MINIO_SECRET_KEY=os.environ.get("MINIO_SECRET_KEY")
-MINIO_BUCKET=os.environ.get("MINIO_BUCKET")
-MODEL_NAME=os.environ.get("MODEL_NAME")
+    # ElasticSerch variables used by elsticsearch service
+    ELASTIC_CLUSTER: List[str]
+    ELASTIC_PRT: str
+    ELASTIC_USR: str
+    ELASTIC_PSW: str
+    ELASTIC_PAGE_SIZE: int
+
+    # MinIO variables used by MinIO service
+    MINIO_URL: str
+    MINIO_ACCESS_KEY: str
+    MINIO_SECRET_KEY: str
+    MINIO_BUCKET: str
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+
+
+settings = Settings()
