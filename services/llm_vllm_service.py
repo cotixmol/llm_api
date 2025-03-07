834c9c76549f7ddebf882fe443c4c2402c1753f3
Ezequiel <eze.munoz@mi.unc.edu.ar>
2025-03-07 16:29:51 -0300

    removed unnecessary prints an logs

--- services/llm_vllm_service.py
+++ services/llm_vllm_service.py
@@ -3,8 +3,6 @@ from vllm import LLM, SamplingParams
 from api.config.logger import logger
 from typing import Optional, Dict, List
 from api.config.secrets import settings as s
-import multiprocessing
-import torch.multiprocessing as mp
 import os
 
 
@@ -13,8 +11,6 @@ class LLMException(Exception):
 
 class LLMService:
     def __init__(self, model_path: str) -> None:
-        print("Current start method:", multiprocessing.get_start_method())
-        print("Current start method:", mp.get_start_method())
         vllm_method = os.environ.get("VLLM_WORKER_MULTIPROC_METHOD", "Not Set")
         print("VLLM_WORKER_MULTIPROC_METHOD:", vllm_method)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
