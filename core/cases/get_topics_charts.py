effcc73e35fea332b734de5b3b28729315a94372
MartinBarran <marrandeguy@gmail.com>
2025-03-20 16:01:12 -0300

    Debugging

--- core/cases/get_topics_charts.py
+++ core/cases/get_topics_charts.py
@@ -71,7 +71,7 @@ class GetTopicChartsCase:
                 query = self.query_repository, 
                 index_pattern=self.index_pattern, 
                 max_ndocs=self.max_ndocs)
-            print(f"#######Documents list: {documents_list}")
+            #print(f"#######Documents list: {documents_list}")
         finally:
             ### CLOSE CLIENT ###
             await self.es_repository.close_client()
