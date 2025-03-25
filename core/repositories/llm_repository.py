809cb397df1dc2900d4d8707d221ffad5cd986d1
MartinBarran <marrandeguy@gmail.com>
2025-03-25 14:24:53 -0300

    Fixed import

--- core/repositories/llm_repository.py
+++ core/repositories/llm_repository.py
@@ -2,7 +2,7 @@ import typing
 import json
 import logging
 from services.llm_vllm_service import LLMService
-from core.repositories.utils import monitor
+from core.repositories.utils.monitor_llm import monitor
 from typing import List, Dict
 from collections import defaultdict
 from api.config.logger import logger
