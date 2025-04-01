6284b2a454e495d422a0b7541743f8d8df8ed3f4
MartinBarran <marrandeguy@gmail.com>
2025-04-01 13:23:07 -0300

    Fixed app.include_router

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ app.add_middleware(
 
 app.include_router(router=topic_router, prefix=('/topics'), tags=["Topics"])
 app.include_router(router=llm_router, prefix=('/llm'), tags=["LLM"])
+app.include_router(router=topic_router, prefix=('/vectorsearch'), tags=["VectorSearch"])
 
 if __name__ == "__main__":
     import uvicorn
