49d6f43f75687be49e66e2653dd3dc8634cecf0f
MartinBarran <marrandeguy@gmail.com>
2025-04-03 11:13:40 -0300

    Testing search instead of knn_search

--- services/elasticsearch_service.py
+++ services/elasticsearch_service.py
@@ -140,7 +140,7 @@ class ElasticsearchService:
     ) -> SearchResponse:
         try:
             # Ejecutamos la consulta KNN pasando el cuerpo (query) directamente.
-            search_results = await self.client.options(request_timeout=10).knn_search(
+            search_results = await self.client.options(request_timeout=10).search( #knn_search
                 index=index_pattern,
                 knn=query,
                 size=size,
