b4bb70421f7773402e6d5570717f40b273462752
Constancio Molinengo <constancio.molinengo@outlook.com>
2025-05-07 10:44:01 -0300

    feat / llm service fake

--- V2/core/factories/classification_repository_factory.py
+++ V2/core/factories/classification_repository_factory.py
@@ -4,7 +4,9 @@ from V2.core.services.document_search_service.elastic_search_service_repository 
 )
 from V2.core.services.llm_service.vllm_service_repository import VLLMServiceRepositoryV2
 from V2.core.repositories.classification_repository import ClassificationRepository
-from V2.core.factories.elastic_search_service_factory import build_es_service
+from V2.core.factories.elasticsearch.elastic_search_service_factory import (
+    build_es_service,
+)
 from V2.core.factories.llm.llm_service_factory import (
     build_vllm_service,
 )
